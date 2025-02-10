efdbefe4abf9a8323257f8566fcc9667416854eb
Leo-sailor <leo.yates@reddamhouse.org.uk>
2025-02-10 14:05:52 +0000

    fallingObject.py done, and lots of new vector methods

--- baseClasses.py
+++ baseClasses.py
@@ -1,4 +1,50 @@
-class Vector:
+import math
+
+
+class Vector2:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+    def __add__(self, other):
+        if isinstance(other, Vector2):
+            return Vector2(self.x + other.x, self.y + other.y)
+    def __abs__(self):
+        return (self.x ** 2 + self.y ** 2) ** 0.5
+    def __sub__(self, other):
+        if isinstance(other, Vector2):
+            return Vector2(self.x - other.x, self.y - other.y)
+    def __mul__(self, scalar):
+        return Vector2(self.x * scalar, self.y * scalar)
+    def __truediv__(self, scalar):
+        return Vector2(self.x / scalar, self.y / scalar)
+    def __str__(self):
+        return f"Vector({self.x}, {self.y})"
+    def __repr__(self):
+        return f"Vector({self.x}, {self.y})"
+    def __eq__(self, other):
+        if isinstance(other, Vector2):
+            return self.x == other.x and self.y == other.y
+        return False
+    def dot(self, other):
+        if isinstance(other, Vector2):
+            return self.x * other.x + self.y * other.y
+        return None
+    def __len__(self):
+        return abs(self)
+    def normalize(self):
+        magnitude = abs(self)
+        if magnitude > 0:
+            return self / magnitude
+    def direction(self):
+        rad = math.atan(self.x/self.y)
+        deg = rad*180/math.pi
+        return deg
+def dir_to_vector2(direction: float, magnitude: float) -> Vector2:
+    # compass direction
+    direction /= 180/math.pi
+    if magnitude < 0:
+        direction -=math.pi
+        magnitude*=-1#
+    x = math.sin(direction) * magnitude
+    y = math.cos(direction) * magnitude
+    return Vector2(x,y)
--- fallingObject.py
+++ fallingObject.py
@@ -1,7 +1,32 @@
-from baseClasses import Vector
-GRAVITY = 9.806
+import math
+from baseClasses import Vector2, dir_to_vector2
 
-class FallingObject:
-    def __init__(self, initial_position:Vector, initial_velocity:Vector):
+GRAVITY = 9.806
+DENSITY = 1.225 #kg m^-3
+PIXELS_PER_METER = 100
+
+class FallingObject():
+    def __init__(self, initial_position:Vector2, initial_velocity:Vector2,largest_dimension:float = 10, drag_coef: float = 0, mass:float = 1):
+        # drag_coef = cross sec area * drag coefficent
         self.position = initial_position
         self.velocity = initial_velocity
+        self.drag_coef = drag_coef
+        self.mass = mass
+        self.large_dim = largest_dimension
+    def force_update_position(self, position) -> None:
+        self.position = position
+    def iterate(self,time: float) -> bool:
+        acceleration = Vector2(0, -GRAVITY)
+        drag_force = (abs(self.velocity)**2) * self.drag_coef * 0.5 * DENSITY
+        acceleration += dir_to_vector2(-self.velocity.direction(), drag_force / self.mass)
+        new_velocity =  acceleration * time
+        self.position += (self.velocity+ new_velocity) * time * 0.5
+        self.velocity += acceleration * time
+        return self.position.y >= -self.large_dim
+
+class Ball(FallingObject):
+    def __init__(self, initial_position:Vector2, initial_velocity:Vector2, radius:float, mass:float):
+        # radius in meters
+        # mass > 0
+        super().__init__(initial_position, initial_velocity,2*radius * PIXELS_PER_METER, math.pi * radius*radius * 0.5, mass)
+        self.radius = radius
